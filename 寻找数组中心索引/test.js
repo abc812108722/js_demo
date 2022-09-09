b952c7ffb2e7db2b83334ca698eb0325fa0be667
honrique <812108722@qq.com>
2022-09-09 16:27:11 +0800

    Create test.js

---
+++ 寻找数组中心索引/test.js
@@ -0,0 +1,25 @@
+// 给一个整数数组 nums ，请计算数组的 中心下标 。
+
+// 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+
+// 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+
+// 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1
+
+var pivotIndex = function(nums) {
+    var sum = 0;
+    var leftnum = 0;
+    for( var i = 0; i< nums.length; i++){
+        sum += nums[i];
+
+    }
+    for (var j = 0; j < nums.length; j++){
+          sum -= nums[j];
+          if (leftnum == sum) {
+            return j;
+          }
+          leftnum += nums[j];
+      }
+      return -1;
+
+};
